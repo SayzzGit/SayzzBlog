a909979cd6c48bf5cd615e36cd70f645a13db8e4
Sayzz <inbox.sayzz@gmail.com>
2025-02-09 02:05:58 +0530

    hngh

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include 'includes/Head.php';
+    ?>
+
+</head>
+<body>
+
+
+
+
+
+
+    <?php include 'includes/Foot.php';
+    ?>
+</body>
+</html>
+
+
+<?php
+
+?>
