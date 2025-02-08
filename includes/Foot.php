80daaf2a10adfb005e3d11cfc0315f0261ca71fa
Sayzz <inbox.sayzz@gmail.com>
2025-02-09 02:01:38 +0530

    First page

---
+++ includes/Foot.php
@@ -0,0 +1,2 @@
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
